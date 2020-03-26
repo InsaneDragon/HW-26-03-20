02e2268157566527ed9146e17ace7400e6db9ed7
C#Developer <shahzodkiller11@gmail.com>
2020-03-26 23:12:03 +0500

    Its Mine

--- Program.cs
+++ Program.cs
@@ -5,83 +5,77 @@ namespace lesson3
     {
         static void Main()
         {
-//My Program
-            decimal userBalance = 300m;
-            string basket = "";
-            string erroBalanceMessage = "У вас недостаточно средств";
+    //         //////////////////////////////////////////////2
+    //         double money=600;
+    //         if(money>500)
+    //         {
+    //             money-=money*0.03;
+    //         }
+    //         else if(money>1000)
+    //         {
+    //             money-=money*0.05;
+    //         }
+    //         System.Console.WriteLine(money);
 
-        start: //Метка "start"
-
-            System.Console.WriteLine($"Ваш баланс - {userBalance}");
-
-            if (userBalance <= 0)
-            {
-                System.Console.WriteLine(erroBalanceMessage);
-                goto End;
-            };
-
-            System.Console.WriteLine("" +
-            "1 Кола - 10 сомон\n" +
-            "2 Сникерс - 7 сомон\n" +
-            "3 Пополнить баланс\n" +
-            "6 Выход\n" +
-            "Ваш выбор (1-6):");
-
-            int choice = int.Parse(Console.ReadLine());
-
-            // goto start; //идти к метке "start"
-            switch (choice)
-            {
-                case 1:
-                    {
-                        if (userBalance - 10 >= 0)
-                        {
-                            basket += "Кола,";
-                            userBalance -= 10;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить или нет? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    case 2:{//Покупка
-                        if (userBalance - 7 >= 0)
-                        {
-                            basket += "Сникерс,";
-                            userBalance -= 7;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить или нет? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    case 3:{//Пополнение
-                        System.Console.WriteLine("You can full it with Alif wallet or Alif card\n Enter your mubners:");
-                        Console.ReadLine();
-                        System.Console.WriteLine("Enter a number:");
-                        decimal money = decimal.Parse(Console.ReadLine());
-                        userBalance += money;
-                        System.Console.Write("Do you want to continue? y/n:");
-                        System.Console.WriteLine(userBalance);
-                        if (Console.ReadLine().ToLower() == "y") goto start;
-                        else goto End;
-                    }
-                case 6: goto End;
-                default: goto start;
-                break;
-            }
-
-        End:
-            System.Console.WriteLine($"Ваши покупки:{basket}\nВаш баланс:{userBalance}");
-            Console.ReadKey();
-        }
+    //         /////////////////////////////////////////////////////3
+    //         double a=5,b=6,c=7,d=8,min=a;
+    //         if(a<b&&b<c&&c<d)
+    //         {
+    //             System.Console.WriteLine("Numbers go by increasing");
+    //         }
+    //         else 
+    //         {
+    //     if (min>b )
+    //     {
+    //       min=b;  
+    //     }
+    //      if(min>c )
+    //    {
+    //       min=c;
+    //    }
+    //     if(min>d)
+    //    {
+    //       min=d;
+    //    }
+    //    else if (a==b&&a==c&&a==d)
+    //    {
+    //        Console.WriteLine(a*b*c*d);
+    //    }
+    //         }
+    /////////////////////////////////////4
+    int a=9, b=10,c=11,max=a,min=a,middle=a;
+  if(max<b)
+ {
+max=b;
+ }
+   if(max<c)
+ {
+max=c;
+ }
+if(min>b)
+{
+    min=b;
+}
+if(min>c)
+{
+    min=c;
+}
+if(a!=max&&a!=min)
+{
+    middle=a;
+}
+if(b!=max&&b!=min)
+{
+    middle=b;
+}
+if(c!=max&&c!=min)
+{
+    middle=c;
+}
+a=max;
+b=middle;
+c=min;
+    System.Console.WriteLine($"a:{a},b:{b},c:{c}");
     }
+}
 }
