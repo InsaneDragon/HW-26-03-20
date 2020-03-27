37be005e19358097eec0a6bc388f48a491c55b15
C#Developer <shahzodkiller11@gmail.com>
2020-03-27 13:12:49 +0500

    EditTheLast

--- Program.cs
+++ Program.cs
@@ -1,81 +1,81 @@
-﻿using System;
-namespace lesson3
-{
-    public class Programm
-    {
-        static void Main()
-        {
-    //         //////////////////////////////////////////////2
-    //         double money=600;
-    //         if(money>500)
-    //         {
-    //             money-=money*0.03;
-    //         }
-    //         else if(money>1000)
-    //         {
-    //             money-=money*0.05;
-    //         }
-    //         System.Console.WriteLine(money);
+﻿// using System;
+// namespace lesson3
+// {
+//     public class Programm
+//     {
+//         static void Main()
+//         {
+//             //////////////////////////////////////////////2
+//             double money=600;
+//             if(money>1000)
+//             {
+//                 money-=money*0.03;
+//             }
+//             else if(money>500)
+//             {
+//                 money-=money*0.05;
+//             }
+//             System.Console.WriteLine(money);
 
-    //         /////////////////////////////////////////////////////3
-    //         double a=5,b=6,c=7,d=8,min=a;
-    //         if(a<b&&b<c&&c<d)
-    //         {
-    //             System.Console.WriteLine("Numbers go by increasing");
-    //         }
-    //         else 
-    //         {
-    //     if (min>b )
-    //     {
-    //       min=b;  
-    //     }
-    //      if(min>c )
-    //    {
-    //       min=c;
-    //    }
-    //     if(min>d)
-    //    {
-    //       min=d;
-    //    }
-    //    else if (a==b&&a==c&&a==d)
-    //    {
-    //        Console.WriteLine(a*b*c*d);
-    //    }
-    //         }
-    /////////////////////////////////////4
-    int a=9, b=10,c=11,max=a,min=a,middle=a;
-  if(max<b)
- {
-max=b;
- }
-   if(max<c)
- {
-max=c;
- }
-if(min>b)
-{
-    min=b;
-}
-if(min>c)
-{
-    min=c;
-}
-if(a!=max&&a!=min)
-{
-    middle=a;
-}
-if(b!=max&&b!=min)
-{
-    middle=b;
-}
-if(c!=max&&c!=min)
-{
-    middle=c;
-}
-a=max;
-b=middle;
-c=min;
-    System.Console.WriteLine($"a:{a},b:{b},c:{c}");
-    }
-}
-}
+//             /////////////////////////////////////////////////////3
+//             double a=5,b=6,c=7,d=8,min=a;
+//             if(a<b&&b<c&&c<d)
+//             {
+//                 System.Console.WriteLine("Numbers go by increasing");
+//             }
+//             else 
+//             {
+//         if (min>b )
+//         {
+//           min=b;  
+//         }
+//          if(min>c )
+//        {
+//           min=c;
+//        }
+//         if(min>d)
+//        {
+//           min=d;
+//        }
+//        else if (a==b&&a==c&&a==d)
+//        {
+//            Console.WriteLine(a*b*c*d);
+//        }
+//             }
+//     ///////////////////////////////////4
+//     int a=9, b=10,c=11,max=a,min=a,middle=a;
+//   if(max<b)
+//  {
+// max=b;
+//  }
+//    if(max<c)
+//  {
+// max=c;
+//  }
+// if(min>b)
+// {
+//     min=b;
+// }
+// if(min>c)
+// {
+//     min=c;
+// }
+// if(a!=max&&a!=min)
+// {
+//     middle=a;
+// }
+// if(b!=max&&b!=min)
+// {
+//     middle=b;
+// }
+// if(c!=max&&c!=min)
+// {
+//     middle=c;
+// }
+// a=max;
+// b=middle;
+// c=min;
+//     System.Console.WriteLine($"a:{a},b:{b},c:{c}");
+//     }
+// }
+// }
